39b972eaca3f0da69c0af3528dbb77632b1c7636
Allfazzer <seeyouinvalhalla12@gmail.com>
2024-12-08 13:07:01 +0800

    feat: Create the constructor of the Parser

--- Parser.java
+++ Parser.java
@@ -3,4 +3,9 @@ import java.util.List;
 public class Parser {
     // The tokens to be processed by the parser
     private List <Token> tokens;
+
+    // Constructor
+    public Parser(List <Token> tokens) {
+        this.tokens = tokens;
+    }
 }
