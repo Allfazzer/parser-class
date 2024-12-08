6b6acd220ea5d95312c14d4817c616e833b85cc5
Allfazzer <seeyouinvalhalla12@gmail.com>
2024-12-08 13:01:55 +0800

    feat: Add attribute "children" to hold the children of the current node

--- Node.java
+++ Node.java
@@ -3,5 +3,8 @@ import java.util.ArrayList;
 public class Node {
     // Value of the current node
     private String value;
+
+    // Children node of the current node
+    ArrayList <Node> children;
   
 }
