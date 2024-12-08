f9a7d81524cff51be36b359507a950d0729ec4cd
Allfazzer <seeyouinvalhalla12@gmail.com>
2024-12-08 13:13:36 +0800

    feat: Create method that will parse the tokenized sentence by returning a root node of the parse tree

--- Parser.java
+++ Parser.java
@@ -8,4 +8,13 @@ public class Parser {
     public Parser(List <Token> tokens) {
         this.tokens = tokens;
     }
+
+    // Parse sentence
+    public Node parseSentence() {
+        // Create Node for the <Sentence> which will be the root of the tree
+        Node root = new Node("<Sentence>");
+
+        // Return the 
+        return root;
+    }
 }
