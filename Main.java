55793fe6f6aa267ba93137a9d25850e6f983e4e5
Allfazzer <seeyouinvalhalla12@gmail.com>
2024-12-08 12:57:47 +0800

    feat: Use Scanner to obtain a tokenized sentence that will be used for debugging the Parser

--- Main.java
+++ Main.java
@@ -1,5 +1,14 @@
+import java.util.List;
+
 public class Main { 
     public static void main(String[] args) {
-       
+        // Initialize a Scanner
+        Scanner scanner =  new Scanner();
+
+        // Retrieve the tokens from the scanner
+        List <List<Token>> tokens = scanner.getTokens();
+
+        // Retrieve the token table of the first sentence
+        List <Token> tokensOfFirstSentence = tokens.get(1);
     }
 }
